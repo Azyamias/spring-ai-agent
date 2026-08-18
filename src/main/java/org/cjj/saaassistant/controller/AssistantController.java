0a97d04fc984d893ec757708e704103e177a65d0
azyamias <15823173+azyamias@user.noreply.gitee.com>
2026-08-18 15:25:17 +0800

    添加提示词模板

--- src/main/java/org/cjj/saaassistant/controller/AssistantController.java
+++ src/main/java/org/cjj/saaassistant/controller/AssistantController.java
@@ -1,11 +1,11 @@
 package org.cjj.saaassistant.controller;
 
-import org.cjj.saaassistant.tool.CustomTools;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +16,19 @@ import java.util.Map;
 @RestController
 public class AssistantController {
 
+    @Value("classpath:/templates/prompt.txt")
+    private Resource template;
+
     @Autowired
     private ChatClient chatClient;
 
     @GetMapping(value = "/chat", produces = "text/html;charset=utf-8")
     public Flux<String> chat(@RequestParam(value = "msg", defaultValue = "你是谁") String msg) {
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Prompt prompt = promptTemplate.create(Map.of());
         return chatClient
-                .prompt().user(msg)
+                .prompt(prompt)
+                .user(msg)
                 .stream().content();
     }
 }
